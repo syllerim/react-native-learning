a1283e2c65425f069676a7e4c387b5c3a1ae69e6
Mirellys Arteta Davila <martetadavila@anwb.nl>
2018-09-17 23:59:17 +0200

    life cycle examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
+
+// This class you can name it as you want
 
 export default class App extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            backgroundColor: 'blue', 
+            height: 300
+        }
+    }
+
+    // Before the component is render
+    componentWillMount() {
+        this.state({ backgroundColor: 'lime', title: 'Value changed before rendering' })
+    }
+
+    // After 5 secs the component will be render with red
+    componentDidMount() {
+        setTimeout( () => {
+            this.state({ backgroundColor: 'red', title: 'Value changed before rendering' })
+        }, 5000)
+    }
     render() {
         return (
-            <View style={{ backgroundColor: 'red', height: 300 }}>
-                
+            <View style={{ flex: 1, backgroundColor: 'red' }}>
+                <View style={{ flex: 1, backgroundColor: 'green' }}></View>
+                <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
             </View>
         )
     }
