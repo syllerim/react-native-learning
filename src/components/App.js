24c26fc340a3c3122b8fc6198bf5f4ea9aa8201b
Mirellys Arteta Davila <martetadavila@anwb.nl>
2018-09-18 21:42:28 +0200

    add react-native-router-flux dependency and make it work with a route

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native'
-
-// This class you can name it as you want
-
+import { Router, Scene, Actions, Stack } from 'react-native-router-flux'
+import { Houses } from './sections/'
 export default class App extends Component {
-    constructor(props) {
-        super(props) 
-        this.state = {
-            backgroundColor: 'blue', 
-            height: 300
-        }
-    }
-
-    // Before the component is render
-    componentWillMount() {
-        this.state({ backgroundColor: 'lime', title: 'Value changed before rendering' })
-    }
-
-    // After 5 secs the component will be render with red
-    componentDidMount() {
-        setTimeout( () => {
-            this.state({ backgroundColor: 'red', title: 'Value changed before rendering' })
-        }, 5000)
-    }
     render() {
         return (
-            <View style={{ flex: 1, backgroundColor: 'red' }}>
-                <View style={{ flex: 1, backgroundColor: 'green' }}></View>
-                <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
-            </View>
+            <Router>
+                <Stack key='root'>
+                    <Scene key='houses' component={Houses} title='Casas' initial={true}/>
+                </Stack>
+            </Router>
         )
     }
 }
---
+++ src/components/sections/index.js
@@ -0,0 +1,6 @@
+//Export all our sessions easily
+//If we don't do this then we have to do on the other files import {Houses from 'long route to houses'}
+
+import Houses from './houses/view'
+
+export { Houses }
